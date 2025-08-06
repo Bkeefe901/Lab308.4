29aceb65220d9f73b518af6db163021fdfc6a155
Bkeefe901 <bradyjokeefe@gmail.com>
2025-08-06 17:27:43 -0400

    Saving before changing to nested loop idea in 3

--- script.js
+++ script.js
@@ -12,16 +12,18 @@
 
 // We need a string to hold the characters for each cell that can also be recycled after each , we push this to the 'row.
 
+console.log(`This is part 1////////////////////////////`);
+
 let str = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26;`
  
 let row = [];
-let ourArray = [];
+let ourArray = []; // second array to hold the row arrays
 let items = ``;
 
-for(let i = 0; i < str.length; i++){
-    if(str[i] == `\n`){
-        row.push(items);
-        ourArray.push(row);
+for(let i = 0; i < str.length; i++){ // loop cycles through every character in str
+    if(str[i] == `\n`){ 
+        row.push(items); 
+        ourArray.push(row); 
         row = [];
         items = ``;
     }
@@ -44,7 +46,7 @@ console.log(ourArray);
 
 
 // Part 2: Expanding Functionality
-
+console.log(`This is Part 2////////////////////////`);
 
 // Begin with the following task:
 
@@ -56,7 +58,7 @@ console.log(numColumns);
 // // Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
 
 
-
+// the code I did for part 1 was already optimized for any number of columns so it works for part 2:
 
 row = [];
 ourArray = [];
@@ -86,7 +88,7 @@ for(let i = 0; i < str.length; i++){
 
 }
 
-// I realized the way I did part 1 already satisfies part 2. 
+
 
 
 
@@ -95,86 +97,77 @@ for(let i = 0; i < str.length; i++){
 
 
 
+console.log(`This is Part 3////////////////////////`);
 
-
-
-
-// Implement the following:
 
 // For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value’s column.
-
 // Convert these keys to all lowercase letters for consistency.
-
 // Store these objects in an array, in the order that they were originally listed.
  
 // Basic Outline
 // // Remove first array in ourArray using .shift and store in new variable called 'Keys'
 // // convert all the strings in the array 'Keys' to lower case and save it to array variable 'keys (lower case k) using forEach method
-let Keys = ourArray.shift();
 
-let keys = [];
+let Keys = ourArray.shift(); // removes first array in ourArray and stores that array in new variable
+
+let keys = []; // created variable for lower case 
 
 Keys.forEach(element => {
-    keys.push(element.toLowerCase())
+    keys.push(element.toLowerCase())  //converts to lowercase
 });
 
-console.log("keys are: ", keys);
-// // Final output will be 4 objects inside an array
-// // // Create loop to create the 4 objects
-// // // // Or should i just create variables for the 4 objects? 
-// // // // // or one object that we push to an array 4 times?
+console.log("keys are: ", keys); // test
 
-let rowObject = {};
+// // Final output will be 4 objects inside an array
+// // // Create an object that has the values from 'keys' as the keys and set the values to; ''
+
+let rowObject = {}; // initializing the basic object
 
 // // // // each object has the keys made up of items from first array in 'ourArray'
 let keyItem = '';
-for(let i = 0; i < keys.length; i++){
+for(let i = 0; i < keys.length; i++){ // loop to pull values from keys and creates key:value pairs in rowObject
     keyItem = keys[i];
     rowObject[keyItem] = '';
 }
-console.log(rowObject);
+console.log(`Test: `, rowObject); //test
 
 
 // // // // declare array variable that hold our objects and be the final output
 
-let objectArray = [];
+let objectArray = []; // initialize the Array to hold our objects
 
-// // // // Flaten ourArray and save to new variable and create loop that iterates through it to fill the vaules of the object and push it to 'object Array' for every row.
-// // // // Also create a variable 'numIterations' that divides the length of our flatened array by keys so that we know how many objects to create and push
+// // // // Flaten ourArray and save to new variable and create loop that iterates through it to fill the vaules of the object and push it to 'object Array' everytime the object gets filled with values
 
 let flatArray = ourArray.flat();
 
-numIterations = flatArray.length / keys.length;
 
-let item = '';
+let item = ''; 
 
-let counterArray = [];
+let a = 0; //initialize counter for item number in objects
 
-let a = 0;
 
-for( let i = 0; i < numIterations; i++){
-      
-    if(a < keys.length){
+
+for(let i = 0; i < flatArray.length; i++){ //loop to iterate through each item in flatArray
+    if(a == keys.length){ //condition to see if the number of items stored in rowObject = keys.length
+        objectArray.push(rowObject);
+        a = 0;
+        i = i - 1;
+    }
+    else{
         item = keys[a];
-        counterArray.push(item);
-        rowObject[item] = flatArray[a];
-        a = counterArray.length;
-    }
-     else{
-          objectArray.push(rowObject);
-          flatArray.slice(0, a);
-          item = '';
-          counterArray = [];
-          rowObject = {};
-          a = 0;
-    }
-}
+        rowObject[item] = flatArray[i];
+        a++;
 
-console.log("counterArray length is ",  a);
-console.log("objectArray is ",  objectArray);
-console.log("rowObject is ",  rowObject);
-console.log("objectArray is ", objectArray);
-console.log(objectArray.push(rowObject));
+    }
+
+}
+console.log(`The object array is: ${objectArray}`);
+
+// console.log("counterArray length is ",  a);
+// console.log("objectArray is ",  objectArray);
+// console.log("rowObject is ",  rowObject);
+// console.log("objectArray is ", objectArray);
+// console.log(objectArray.push(rowObject));
 // // // // Must iterate through ourArray to grab items from other arrays to grab the values
 // // // // // Should this use one loop or a different loop
 
@@ -192,6 +185,8 @@ objectArray = [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
                 { id: "98", name: "Bill", occupation: "Doctor’s Assistant", age: "26" }];
 
 // Part 4: Sorting and Manipulating Data
+
+console.log(`This is Part 4////////////////////////`);
 
 // // Using array methods, accomplish the following tasks, in order upon the result of Part 3:
 
@@ -241,6 +236,9 @@ console.log(`The average age is: ${avgAge}`);
 
 // Part 5: Full Circle
 
+
+console.log(`This is Part 5////////////////////////`);
+
 // // As a final task, transform the final set of data back into CSV format.
 
 // // Basic Outline:
@@ -279,12 +277,12 @@ console.log(valuesArray);
 
 let flatendArray = valuesArray.flat()
 console.log(flatendArray);
-
+let counter = 1;
 
 for(let i = 0; i < flatendArray.length; i++){
-    
-    if((i + 1) % keyArray.length == 0){
-        flatendArray.splice((i + 1), 0, '\n');
+    if((i + counter) % keyArray.length == 0){
+        flatendArray.splice((i + counter), 0, '\n');
+        counter += 1;
     }
 }
 console.log(flatendArray.length);
